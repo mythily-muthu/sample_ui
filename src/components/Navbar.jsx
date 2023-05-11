d5dece37c17b85f724b69107005a5f0ec1dedc42
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-11 13:38:41 +0530

    Navbar title added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-customblue h-36 w-full flex">
-      <div className=""></div>
+    <div className="w-full bg-customblue flex justify-center">
+      <div className=" h-36 w-[80%] flex p-8 items-center text-yellow-50 justify-between ">
+        <h1 className="text-3xl font-semibold cursor-pointer">
+          Select A THEME
+        </h1>
+        <ul className="flex gap-4 cursor-pointer text-lg w-[fit-content]">
+          <li className="hover:font-bold">Business</li>
+          <li className="hover:font-bold">Portfolio</li>
+          <li className="hover:font-bold">Personal</li>
+          <li className="hover:font-bold">Event</li>
+          <li className="hover:font-bold">Blog</li>
+          <li className="hover:font-bold">Comig soon</li>
+          <li className="hover:font-bold">Others</li>
+        </ul>
+      </div>
     </div>
   );
 };
